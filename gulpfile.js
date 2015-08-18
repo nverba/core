c3fc0744a97fafd7478445d28ff95778b2a9547c
nverba <distantstate@gmail.com>
2015-08-18 09:40:55 +0100

    BrowserSync working

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ gulp.task('build:static', ['build:img', 'build:json']);
 
 gulp.task('build', ['flush:js', 'build:img', 'build:json', 'build:css', 'build:templates', 'build:main', 'build:libs', 'build:index', 'build:static']);
 
-gulp.task('build:html', ['build:css', 'build:templates', 'build:index']);
+gulp.task('build:html', ['build:templates', 'build:index']);
 
 gulp.task('watch:html', ['build:html'], function() {
   return gulp.watch(['./src/**/*.scss', './src/**/*.html'], ['build:html']);
@@ -21,8 +21,8 @@ gulp.task('browser-sync', ['nodemon'], function() {
 });
 
 gulp.task('serve', ['build:css', 'browser-sync'], function () {
-    gulp.watch('./src/**/*.scss', '!./src/scss/immediate.scss', ['build:css']);
-    gulp.watch(["./src/**/*.js", "./src/**/*.html"], reload);
+    gulp.watch('./src/**/*.scss', ['build:css']);
+    gulp.watch(["./src/**/*.js", "./src/**/*.html"], ['build:html', reload]);
 });
 
 gulp.task('nodemon', function (cb) {
@@ -33,39 +33,4 @@ gulp.task('nodemon', function (cb) {
             cb();
         }
     });
-});
-
-
-
-var sass         = require('gulp-sass');
-var inject       = require('gulp-inject');
-var autoprefixer = require('gulp-autoprefixer');
-var freeze       = require('gulp-freeze');
-var streamify    = require('gulp-streamify');
-var gulpif       = require('gulp-if');
-var argv         = require('yargs').argv;
- var del          = require('del');
-
-gulp.task('build:css', function () {
-  
-  return del(argv.production ? 'public/css/*.*' : 'build/css/*.*', function() {
-    
-    // Collect all .scss file names from components and inject into main.scss as dependencies
-    return gulp.src('./src/scss/main.scss')
-      .pipe(inject(
-        gulp.src(['./src/**/*.scss', '!./src/scss/immediate.scss', '!./src/scss/main.scss'], {read: false}), {
-          starttag: '/* {{name}}:{{ext}} */',
-          endtag: '/* endinject */',
-          transform: function (filepath) {
-            return filepath === 'main.scss' ? '' : '@import "../..' + filepath + '";';
-          }
-        }
-      ))
-  	  .pipe(streamify(sass()))
-      .pipe(autoprefixer())
-      .pipe(gulpif(argv.production, freeze()))
-      .pipe(gulpif(argv.production, gulp.dest('./public/css'), gulp.dest('./build/css')))
-      .pipe(reload({stream:true}));
-      
-  });
 });
