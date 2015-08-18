e7bdb846811a50b654644c56e32fe15cacd0bb48
nverba <distantstate@gmail.com>
2015-08-18 11:16:52 +0100

    fixup browsersync - still issues with ie9 virtual machine

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,16 @@ var requireDir = require('require-dir')('./tasks');
 var browserSync  = require('browser-sync');
 var reload = browserSync.reload;
 var nodemon = require('gulp-nodemon');
+var gutil = require('gulp-util');
+
+var sass         = require('gulp-sass');
+var inject       = require('gulp-inject');
+var autoprefixer = require('gulp-autoprefixer');
+var freeze       = require('gulp-freeze');
+var streamify    = require('gulp-streamify');
+var gulpif       = require('gulp-if');
+var argv         = require('yargs').argv;
+var del          = require('del');
 
 gulp.task('build:static', ['build:img', 'build:json']);
 
@@ -16,11 +26,19 @@ gulp.task('watch:html', ['build:html'], function() {
 
 gulp.task('browser-sync', ['nodemon'], function() {
     browserSync.init(null, {
-        proxy: "localhost:8080"
+        proxy: {
+          target: "localhost:8080",
+          middleware: function (req, res, next) {
+            setTimeout(function() {
+              next();
+            }, 100)
+          }
+        }
+
     });
 });
 
-gulp.task('serve', ['build:css', 'browser-sync'], function () {
+gulp.task('serve', ['build:css', 'browser-sync', 'build:main'], function () {
     gulp.watch('./src/**/*.scss', ['build:css']);
     gulp.watch(["./src/**/*.js", "./src/**/*.html"], ['build:html', reload]);
 });
@@ -33,4 +51,34 @@ gulp.task('nodemon', function (cb) {
             cb();
         }
     });
+});
+
+
+
+
+
+gulp.task('build:css', function () {
+  
+  return del(argv.production ? 'public/css/*.*' : 'build/css/*.*', function() {
+    
+    // Collect all .scss file names from components and inject into main.scss as dependencies
+    return gulp.src('./src/scss/main.scss')
+      .pipe(inject(
+        gulp.src(['./src/**/*.scss', '!./src/scss/immediate.scss', '!./src/scss/main.scss'], {read: false}), {
+          starttag: '/* {{name}}:{{ext}} */',
+          endtag: '/* endinject */',
+          transform: function (filepath) {
+            return filepath === 'main.scss' ? '' : '@import "../..' + filepath + '";';
+          }
+        }
+      ))
+  	  .pipe(streamify(sass()))
+      .pipe(gutil.noop(gutil.log('!!!!!!!!!!!!!!!!!!!!!!after inject')))
+      .pipe(autoprefixer())
+      .pipe(gulpif(argv.production, freeze()))
+      .pipe(gulpif(argv.production, gulp.dest('./public/css'), gulp.dest('./build/css')))
+      .pipe(browserSync.stream())
+      .pipe(gutil.noop(gutil.log('!!!!!!!!!!!!!!!!!!!!!!after browser sync')));
+      
+  });
 });
--- tasks/build-css.js
+++ tasks/build-css.js
@@ -1,35 +1,45 @@
-var gulp         = require('gulp');
-var sass         = require('gulp-sass');
-var inject       = require('gulp-inject');
-var autoprefixer = require('gulp-autoprefixer');
-var freeze       = require('gulp-freeze');
-var streamify    = require('gulp-streamify');
-var gulpif       = require('gulp-if');
-var del          = require('del');
-var argv         = require('yargs').argv;
-var browserSync  = require('browser-sync');
-var reload = browserSync.reload;
+// var gulp         = require('gulp');
+// var sass         = require('gulp-sass');
+// var inject       = require('gulp-inject');
+// var autoprefixer = require('gulp-autoprefixer');
+// var freeze       = require('gulp-freeze');
+// var streamify    = require('gulp-streamify');
+// var gulpif       = require('gulp-if');
+// var del          = require('del');
+// var argv         = require('yargs').argv;
+// var browserSync  = require('browser-sync');
+// var reload = browserSync.reload;
 
-gulp.task('build:css', function () {
+
+// var sass         = require('gulp-sass');
+// var inject       = require('gulp-inject');
+// var autoprefixer = require('gulp-autoprefixer');
+// var freeze       = require('gulp-freeze');
+// var streamify    = require('gulp-streamify');
+// var gulpif       = require('gulp-if');
+// var argv         = require('yargs').argv;
+// var del          = require('del');
+
+// gulp.task('build:css', function () {
   
-  return del(argv.production ? 'public/css/*.*' : 'build/css/*.*', function() {
+//   return del(argv.production ? 'public/css/*.*' : 'build/css/*.*', function() {
     
-    // Collect all .scss file names from components and inject into main.scss as dependencies
-    return gulp.src('./src/scss/main.scss')
-      .pipe(inject(
-        gulp.src(['./src/**/*.scss', '!./src/scss/immediate.scss', '!./src/scss/main.scss'], {read: false}), {
-          starttag: '/* {{name}}:{{ext}} */',
-          endtag: '/* endinject */',
-          transform: function (filepath) {
-            return filepath === 'main.scss' ? '' : '@import "../..' + filepath + '";';
-          }
-        }
-      ))
-  	  .pipe(streamify(sass()))
-      .pipe(autoprefixer())
-      .pipe(gulpif(argv.production, freeze()))
-      .pipe(gulpif(argv.production, gulp.dest('./public/css'), gulp.dest('./build/css')))
-      .pipe(reload({stream:true}));
+//     // Collect all .scss file names from components and inject into main.scss as dependencies
+//     return gulp.src('./src/scss/main.scss')
+//       .pipe(inject(
+//         gulp.src(['./src/**/*.scss', '!./src/scss/immediate.scss', '!./src/scss/main.scss'], {read: false}), {
+//           starttag: '/* {{name}}:{{ext}} */',
+//           endtag: '/* endinject */',
+//           transform: function (filepath) {
+//             return filepath === 'main.scss' ? '' : '@import "../..' + filepath + '";';
+//           }
+//         }
+//       ))
+//   	  .pipe(streamify(sass()))
+//       .pipe(autoprefixer())
+//       .pipe(gulpif(argv.production, freeze()))
+//       .pipe(gulpif(argv.production, gulp.dest('./public/css'), gulp.dest('./build/css')))
+//       .pipe(reload({stream:true}));
       
-  });
-});
+//   });
+// });
